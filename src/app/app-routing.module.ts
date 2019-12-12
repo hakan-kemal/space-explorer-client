84c162e143530343584eb113d23e55f2d4b184cc
hakan0814 <h.k.cuhadar@gmail.com>
2019-12-12 14:12:26 +0100

    Add router module and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { SpaceComponent } from "./space/space.component";
+// import { NgModule } from "@angular/core";
+// import { RouterModule, Routes } from "@angular/router";
+// import { HomeComponent } from "./home/home.component";
+// import { SpaceComponent } from "./space/space.component";
 // import { PlanetComponent } from "./planet/planet.component";
 
-const routes: Routes = [
-  { path: "home", component: HomeComponent },
-  { path: "space", component: SpaceComponent }
-  // { path: "planet", component: PlanetComponent }
-];
+// const routes: Routes = [
+//   { path: "home", component: HomeComponent },
+//   { path: "space", component: SpaceComponent }
+// { path: "planet", component: PlanetComponent }
+// ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
+import { RouterModule, Routes } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
@@ -9,11 +9,16 @@ import { ServersComponent } from "./servers/servers.component";
 import { ServerComponent } from "./server/server.component";
 import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
 import { SuccessAlertComponent } from "./success-alert/success-alert.component";
-import { SpaceComponent } from "./space/space.component";
-import { AppRoutingModule } from "./app-routing.module";
 import { HomeComponent } from "./home/home.component";
+import { SpaceComponent } from "./space/space.component";
 // import { PlanetComponent } from "./planet/planet.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "space", component: SpaceComponent }
+  // { path: "planet", component: PlanetComponent }
+];
 
 @NgModule({
   declarations: [
@@ -22,17 +27,18 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     ServerComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
-    SpaceComponent,
-    HomeComponent
+    HomeComponent,
+    SpaceComponent
     // PlanetComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
-    AppRoutingModule,
     BrowserAnimationsModule
   ],
+  // exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
